96efd77c36ad308ff973041c61b82ca31439d9e0
Nghi Nguyen Van <nghinv@eway.vn>
2016-03-24 07:20:26 +0700

    TQA-2187: add script to run sample tests

---
+++ testsuite/jmeter-util-prepare.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+EXO_WORKING_DIR=/java/exo-working
+
+if [ -d $EXO_WORKING_DIR ]; then
+  pushd $EXO_WORKING_DIR
+  
+  if [ ! -d automation_xss_tc_new ]; then
+    echo "INFO: downloading automation_xss_tc_new"
+    git clone https://github.com/nghinv/automation_xss_tc_new.git 
+    ln -s automation_xss_tc_new automation_xss_tc
+  fi
+  #download firefox
+  if [ ! -f firefox-21.0b7.tar.bz2 ]; then
+    if [ ! -d firefox21 ]; then
+      # remove firefox folder
+      rm -frv current_firefox || true
+      
+      # download firefox21
+      curl -fL https://download-installer.cdn.mozilla.net/pub/firefox/releases/21.0b7/linux-x86_64/en-US/firefox-21.0b7.tar.bz2  -o ${EXO_WORKING_DIR}/firefox-21.0b7.tar.bz2 
+      tar xf ${EXO_WORKING_DIR}/firefox-21.0b7.tar.bz2
+      
+      # choose ff21
+      mv firefox firefox21
+      ln -s firefox21 current_firefox
+    else
+      echo "INFO: the folder firefox21 existing, abort the download process"
+    fi
+  fi
+  
+  if [ ! -f selenium-server-standalone-2.52.0.jar ]; then
+    echo "INFO: downloading selenium-server-standalone-2.52.0.jar"
+    curl -fL http://selenium-release.storage.googleapis.com/2.52/selenium-server-standalone-2.52.0.jar -o ${EXO_WORKING_DIR}/selenium-server-standalone-2.52.0.jar
+  fi
+  
+  if [ ! -f plf-community-tomcat-standalone-4.3.0.zip ]; then
+    echo "INFO: downloading plf-community-tomcat-standalone-4.3.0.zip"
+    curl -fL https://repository.exoplatform.org/service/local/repositories/exo-releases/content/org/exoplatform/platform/distributions/plf-community-tomcat-standalone/4.3.0/plf-community-tomcat-standalone-4.3.0.zip -o ${EXO_WORKING_DIR}/plf-community-tomcat-standalone-4.3.0.zip
+    mkdir ${EXO_WORKING_DIR}/TC || true
+    if [ -d ${EXO_WORKING_DIR}/TC/current ]; then
+      echo "INFO: removing ${EXO_WORKING_DIR}/TC/current"
+      rm -rf ${EXO_WORKING_DIR}/TC/current
+    fi
+    unzip -q ${EXO_WORKING_DIR}/plf-community-tomcat-standalone-4.3.0.zip -d /tmp/PLF
+    TMP_PLF_DIR=`find /tmp/PLF -maxdepth 1 -mindepth 1`
+    if [ $? -eq 0 ]; then
+      mv $TMP_PLF_DIR/* ${EXO_WORKING_DIR}/TC/current/
+      # extract dataset
+      echo "INFO: Extracting dataset" # extract dataset
+      
+      pushd ${EXO_WORKING_DIR}/TC/current/
+      mkdir DATASET
+      pushd DATASET
+      # this dataset contain conf/server.xml and webapps/eXoResources.war together with gatein/data
+      tar xf ${EXO_WORKING_DIR}/automation_xss_tc/plf_templates/datasets/plf43comm/PLF43_DATASET_HSQL.tar.bz2
+      popd
+      cp DATASET/* . -R
+      rm -rf DATASET
+      echo "INFO: ${EXO_WORKING_DIR}/TC/current/ is now ready to run"
+      popd #to EXO_WORKING_DIR
+      
+      echo "${EXO_WORKING_DIR}/TC/current/ RE_USE!SAMPLES 3600 SAMPLE_TEST_WITH_PLF43 SHOULD_WORK!eXoPLF.install.sh"
+    fi
+  fi
+else
+  echo "ERROR: Expect to have the folder $EXO_WORKING_DIR to exist"
+  a=$((1/0))
+fi
